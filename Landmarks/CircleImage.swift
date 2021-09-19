85d95cde3d955a0b8f4f4a18270ba6c05cd682d3
Hugo Streppel Silva <strh93@gmail.com>
2021-09-19 15:50:34 -0300

    added resources

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  CircleImage
+//
+//  Created by Hugo Silva on 19/09/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  MapView
+//
+//  Created by Hugo Silva on 19/09/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
